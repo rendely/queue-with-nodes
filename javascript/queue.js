a6a9a482c22b36d94b3a0d755f9189c4d5815ed8
matthew rendely <matthew.rendely@gmail.com>
2023-12-04 19:47:18 -0800

    done

--- javascript/queue.js
+++ javascript/queue.js
@@ -14,26 +14,52 @@ class Queue {
   // ADD NODE TO BACK OF QUEUE
   // USE DATA TO CREATE A NEW NODE AND ADD IT TO THE QUEUE
   enqueue(data) {
+    const node = new Node(data);
+
+    if (!this.front){
+      this.front = node;
+    }
+    else if (this.rear) {
+      this.rear.next = node;
+    }
+
+    this.rear = node;
 
   }
 
   // REMOVE NODE FROM FRONT OF QUEUE AND RETURN IT
   dequeue() {
+    if (!this.front) return null 
 
+    const node = this.front;
+    this.front = node.next;
+    if (!node.next) this.rear = null;
+
+    return node
   }
 
   // RETURN NODE AT FRONT WITHOUT REMOVING IT
   peek() {
+    return this.front;
 
   }
 
   // RETURN TRUE IF QUEUE IS EMPTY, OTHERWISE FALSE
   isEmpty() {
-
+    return !this.rear
   }
 
   // RETURN NUMBER OF NODES IN QUEUE, E.G. 10
   size() {
+    let count = 0;
+    let node = this.front;
+
+    while (node){
+      count++;
+      node = node.next;
+    }
+
+    return count;
 
   }
 
@@ -41,11 +67,31 @@ class Queue {
   // IF TARGET ISN'T IN QUEUE, RETURN -1
   search(target) {
 
+    let index = 0;
+    let node = this.front;
+
+    while (node){
+      if (node.data === target) return index
+      index++;
+      node = node.next;
+    }
+
+    return -1;
+
   }
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  const one = new  Node(1)
+  const two = new Node(2)
+  const three = new Node(3)
+
+  one.next = two;
+
+  const q = new Queue(one);
+
+  console.log(q.search(2))
+  console.log(q)
  
 }
 
